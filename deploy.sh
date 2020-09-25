0f84cb03e1c6b6de992f12c434d230b3fe9e981e
abhinavjj1 <71740240+abhinavjj1@users.noreply.github.com>
2020-09-24 20:20:47 -0500

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,5 @@
+#! /bin/sh
+
+docker pull registry.transplace.com/tplace/openshift-client:latest
+docker run -it --user=root registry.transplace.com/tplace/openshift-client:latest bash
+bash /tag-to-image-stream.sh
